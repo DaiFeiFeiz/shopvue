ca8614e9a69a1ba5fcb62a3a2153421236fcc8f4
fei <834305234@qq.com>
2019-04-22 15:44:51 +0800

    登录页面完成--token限制访问

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,16 @@
 <template>
-    <div>
-        后台首页
-    </div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style lang="less" scoped>
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,27 @@ import Home from '@/components/Home'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     // {path: '/', component: HelloWorld},
-    {path: '/login', component: Login},
-    {path: '/home', component: Home}
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home
+    }
   ]
 })
+
+// 设置路由守卫
+router.beforeEach((to, from, next) => {
+  var token = window.sessionStorage.getItem('token')
+  if (token === null && to.path !== '/login') {
+    return next('/login')
+  }
+  next()
+})
+
+export default router
